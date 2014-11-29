24287f4f684177a8bd7837d5bfcbff3163d25c0f
Yilin Chen <wormful@gmail.com>
2014-11-29 13:40:09 +0800

    add tasks

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+seed_answer = 33495789
+
 if File.exist?('data.db')
   print 'The database file already exists. Still continue? (Y/N) '
   input = gets
@@ -9,21 +11,21 @@ require './db.rb'
 
 DB.create_table :users do
   primary_key :id
-  String :identifier
+  String :identifier, index: true
   Fixnum :praise
   Fixnum :answer
   Fixnum :follower
   Float :possibility
-  boolean :visited
+  boolean :visited, default: false
   boolean :judgment
 end
 
 DB.create_table :answers do
   primary_key :id
-  String :identifier
+  Fixnum :identifier, index: true
   Fixnum :praise
   Fixnum :pollution
-  boolean :visited
+  boolean :visited, default: false
   boolean :judgment
 end
 
@@ -33,6 +35,18 @@ DB.create_table :users_answers do
   primary_key [:user_id, :answer_id]
 end
 
+DB.create_table :user_tasks do
+  primary_key :id
+  String :identifier, index: true
+  boolean :finished, default: false
+end
+
+DB.create_table :answer_tasks do
+  primary_key :id
+  Fixnum :identifier, index: true
+  boolean :finished, default: false
+end
+
 class User < Sequel::Model
   many_to_many :answers
 end
@@ -40,3 +54,11 @@ end
 class Answer < Sequel::Model
   many_to_many :users
 end
+
+class UserTask < Sequel::Model
+end
+
+class AnswerTask < Sequel::Model
+end
+
+AnswerTask.create(:identifier => seed_answer)
